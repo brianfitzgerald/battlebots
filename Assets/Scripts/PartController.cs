e0d92f3e502bc6bd1f8572befffc1bd548608cf5
Brian Fitzgerald <brian.fitzgerald@equipmentshare.com>
2021-01-10 14:40:13 -0600

    user controller

--- Assets/Scripts/PartController.cs
+++ Assets/Scripts/PartController.cs
@@ -12,4 +12,18 @@ public class PartController : MonoBehaviour
 {
     public Transform anchor;
     public PartType partType;
+    private bool isGhost = false;
+
+    private Material ghostMaterial;
+    private Material partMaterial;
+    public void Start()
+    {
+        ghostMaterial = Resources.Load("Material/Ghost.mat", typeof(Material)) as Material;
+        partMaterial = Resources.Load("Material/Part.mat", typeof(Material)) as Material;
+    }
+    public void SetGhost(bool isGhost)
+    {
+        this.isGhost = isGhost;
+        GetComponent<MeshRenderer>().material = isGhost ? ghostMaterial : partMaterial;
+    }
 }
---
+++ Assets/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct PlaceablePart
+{
+    public GameObject prefab;
+    public string name;
+}
+
+public class UserController : MonoBehaviour
+{
+    private Camera placingCamera;
+    private bool placingPart;
+
+    public List<PlaceablePart> placeableParts = new List<PlaceablePart>();
+
+    public GameObject placingPartGhost;
+
+    public PlaceablePart partBeingPlaced;
+
+    public void Start()
+    {
+        placingCamera = GetComponent<Camera>();
+    }
+    public void Update()
+    {
+        if (placingPart)
+        {
+            RaycastHit hit;
+            Ray ray = placingCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                var r = objectHit.transform.GetComponentInParent<RobotController>();
+                if (r != null)
+                {
+                    if (placingPartGhost == null)
+                    {
+                        placingPartGhost = Instantiate(partBeingPlaced.prefab);
+                        placingPartGhost.GetComponent<PartController>().SetGhost(true);
+                    }
+                    // get the normal of the plane we collided with and use that to orient the part
+                    placingPartGhost.transform.rotation = Quaternion.Euler(hit.normal);
+                    placingPartGhost.transform.position = hit.point;
+                }
+
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                placingPartGhost.GetComponent<PartController>().SetGhost(false);
+            }
+        }
+
+    }
+    // TODO: replace with dynamically adding prefabs
+    public void PlaceBonk()
+    {
+        partBeingPlaced = placeableParts[0];
+        placingPart = true;
+        placingPartGhost = null;
+    }
+}
